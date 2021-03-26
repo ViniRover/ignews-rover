e3e4e856a1a651591d263ce10ba163718c5baecc
ViniRover <viniciusrover3@gmail.com>
2021-03-26 17:51:33 -0300

    Webhooks Stripe, Creating Subscriptions

---
+++ src/pages/api/stripewebhooks.ts
@@ -0,0 +1,82 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import { Stripe } from "stripe";
+import { stripe } from "../../services/stripe";
+import { saveSubscrition } from "./_lib/manageSubscription";
+
+async function buffer(readable: Readable) {
+  const chunks = []
+
+  for await(const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  }
+}
+
+const relevantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+])
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if(request.method === 'POST') {
+    const buff = await buffer(request);
+    const secret = request.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buff, secret, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
+      return response.status(400).send(`Webhook errors: ${err.message}`);
+    }
+
+    const { type } = event;
+
+    if(relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscrition({
+              subscriptionId: subscription.id,
+              customerId: subscription.customer.toString(),
+              createAction: false,
+            });
+          break;
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+            await saveSubscrition({
+              subscriptionId: checkoutSession.subscription.toString(),
+              customerId: checkoutSession.customer.toString(),
+              createAction: true,
+            });
+
+          break;
+          default:
+            throw new Error('Unhandled event.');
+        }
+      } catch (err) {
+        return response.json({ error: 'Webhooj handler failed. '});
+      }
+    }
+
+    response.status(200).json({ recieved: true });
+  } else {
+    response.setHeader('Allow', 'POST');
+    response.status(405).end('Method not allowed');
+  }
+}
