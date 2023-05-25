08c5259e641357d553ab147bcf3d6cddc87ab960
Vinicius Rover <viniciusrover3@gmail.com>
2023-05-25 15:55:55 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,16 @@ export default function Home({ product }: HomeProps) {
     <>
       <Head>
         <title>Home | ig.news</title>
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3508026,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
       </Head>
 
       <main className={styles.contentContainer}>
